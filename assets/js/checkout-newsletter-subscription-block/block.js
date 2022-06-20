0f82dd3c2b89399214ec85e7927809db17d1ceb2
Thomas Roberts <thomas.roberts@automattic.com>
2022-06-20 14:20:34 +0100

    Show validation error if box is left unticked

--- assets/js/checkout-newsletter-subscription-block/block.js
+++ assets/js/checkout-newsletter-subscription-block/block.js
@@ -4,6 +4,7 @@
 import { useEffect, useState } from '@wordpress/element';
 import { CheckboxControl } from '@woocommerce/blocks-checkout';
 import { getSetting } from '@woocommerce/settings';
+import { useSelect, useDispatch } from '@wordpress/data';
 
 const { optinDefaultText } = getSetting( 'newsletter-test_data', '' );
 
@@ -11,18 +12,57 @@ const Block = ( { children, checkoutExtensionData } ) => {
 	const [ checked, setChecked ] = useState( false );
 	const { setExtensionData } = checkoutExtensionData;
 
+	const { setValidationErrors, clearValidationError } = useDispatch(
+		'wc/store/validation'
+	);
+
 	useEffect( () => {
 		setExtensionData( 'newsletter-test', 'optin', checked );
-	}, [ checked, setExtensionData ] );
+		if ( ! checked ) {
+			setValidationErrors( {
+				'newsletter-test': {
+					message: 'Please tick the box',
+					hidden: false,
+				},
+			} );
+			return;
+		}
+		clearValidationError( 'newsletter-test' );
+	}, [
+		clearValidationError,
+		setValidationErrors,
+		checked,
+		setExtensionData,
+	] );
+
+	const { getValidationError } = useSelect( ( select ) => {
+		const store = select( 'wc/store/validation' );
+		return {
+			getValidationError: store.getValidationError(),
+		};
+	} );
+
+	const errorMessage = getValidationError( 'newsletter-test' )?.message;
 
 	return (
-		<CheckboxControl
-			id="subscribe-to-newsletter"
-			checked={ checked }
-			onChange={ setChecked }
-		>
-			{ children || optinDefaultText }
-		</CheckboxControl>
+		<>
+			<CheckboxControl
+				id="subscribe-to-newsletter"
+				checked={ checked }
+				onChange={ setChecked }
+			>
+				{ children || optinDefaultText }
+			</CheckboxControl>
+
+			{ errorMessage && (
+				<div>
+					<span role="img" aria-label="Warning emoji">
+						⚠️
+					</span>
+					{ errorMessage }
+				</div>
+			) }
+		</>
 	);
 };
 
